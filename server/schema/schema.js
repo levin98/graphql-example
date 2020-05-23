df3c31cb51f1ed2b749c3f817cf90ae29eaf1e87
Yu Zhe <kelvinlim_98@live.com>
2020-05-23 20:57:23 +0800

    Updated graphql schema to retrieve data from mongo

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -37,6 +37,7 @@ const BookType = new GraphQLObjectType({
             type: AuthorType,
             resolve(parent, args) {
                 // return _.find(authors, { id: parent.authorId })
+                return Author.findById(parent.authorId)
             }
         }
     })
@@ -52,6 +53,7 @@ const AuthorType = new GraphQLObjectType({
             type: new GraphQLList(BookType),
             resolve(parent, args) {
                 // return _.filter(books, { authorId: parent.id })
+                return Book.find({ authorId: parent.id })
             }
         }
     })
@@ -66,6 +68,7 @@ const RootQuery = new GraphQLObjectType({
             resolve(parent, args) {
                 // To get data from db / other source
                 // return _.find(books, { id: args.id })
+                return Book.findById(args.id)
             }
         },
         author: {
@@ -73,18 +76,21 @@ const RootQuery = new GraphQLObjectType({
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
                 // return _.find(authors, { id: args.id })
+                return Author.findById(args.id)
             }
         },
         books: {
             type: new GraphQLList(BookType),
             resolve(parent, args) {
                 // return books
+                return Book.find({})
             }
         },
         authors : {
             type: new GraphQLList(AuthorType),
             resolve(parent, args) {
                 // return authors
+                return Author.find({})
             }
         }
     }
