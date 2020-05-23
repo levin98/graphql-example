eab056e82a3084aae5df0f1d37434c12fdd3cabc
Yu Zhe <kelvinlim_98@live.com>
2020-05-23 20:29:00 +0800

    Added mongo

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
+const mongoose = require('mongoose')
 
 const app = express()
+
+mongoose.connect('mongodb+srv://yuzhe:yuzhe@cluster0-amuiv.mongodb.net/test?retryWrites=true&w=majority')
+mongoose.connection.once('open', () => {
+    console.log('Connected to database.')
+})
 
 app.use('/graphql', graphqlHTTP({
     schema,
