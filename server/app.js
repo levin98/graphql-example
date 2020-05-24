dd0345e3d4b6e4e6ab53c155b703f623af00839f
Yu Zhe <kelvinlim_98@live.com>
2020-05-24 14:59:55 +0800

    Added cors middleware to server

--- server/app.js
+++ server/app.js
@@ -2,8 +2,11 @@ const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const app = express()
+
+app.use(cors())
 
 mongoose.connect('mongodb+srv://yuzhe:yuzhe@cluster0-amuiv.mongodb.net/test?retryWrites=true&w=majority')
 mongoose.connection.once('open', () => {
