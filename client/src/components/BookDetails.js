99239f5fda42e0f4eafdc58cc8d6903e65176b2d
Yu Zhe <kelvinlim_98@live.com>
2020-05-24 15:55:07 +0800

    Added book details componenet

---
+++ client/src/components/BookDetails.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import {  } from '../queries/queries';
+
+class BookDetails extends Component {
+
+    render() {
+        return (
+            <div id="book-details">
+                <p></p>
+            </div>
+        );
+    }
+}
+
+export default BookDetails;
