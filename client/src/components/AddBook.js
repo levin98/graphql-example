636f29758f38a9d36bec91e6bd410006b31cef06
Yu Zhe <kelvinlim_98@live.com>
2020-05-24 15:42:36 +0800

    Added add book mutation query

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
-import { getAuthorsQuery } from '../queries/queries';
+import compose from "lodash.flowright";
+import { getAuthorsQuery, addBookMutation } from '../queries/queries';
 
 class AddBook extends Component {
     constructor(props) {
@@ -49,4 +50,7 @@ class AddBook extends Component {
     }
 }
 
-export default graphql(getAuthorsQuery)(AddBook);
+export default compose(
+    graphql(getAuthorsQuery, { name: "getAuthorsQuery" }),
+    graphql(addBookMutation, { name: "addBookMutation" })
+)(AddBook);
