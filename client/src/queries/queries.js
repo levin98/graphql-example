908f8a2ba3e05718a33b75ce5d86ac4063119910
Yu Zhe <kelvinlim_98@live.com>
2020-05-24 15:58:06 +0800

    Added get book detail query

--- client/src/queries/queries.js
+++ client/src/queries/queries.js
@@ -29,4 +29,23 @@ const addBookMutation = gql`
     }
 `
 
-export { getBooksQuery, getAuthorsQuery, addBookMutation }
+const getBookQueryById = gql`
+    query ($id: String) {
+        book (id: $id) {
+            id
+            name
+            genre
+            author {
+                id
+                name
+                age
+                books {
+                    id
+                    name
+                }
+            }
+        }
+    }
+`
+
+export { getBooksQuery, getAuthorsQuery, addBookMutation, getBookQueryById }
